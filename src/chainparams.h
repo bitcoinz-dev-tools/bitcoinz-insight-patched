7e174f0b537035dc3985dbf75db7e86481ef9862
renuzit <renuzit@btcz.life>
2019-05-25 12:50:01 -0500

    Support to reduce future block time window to 5 mins
    Check timestamp is within the allowed 5 minute window to help decrease
    effectiveness of timewarp attacks

--- src/chainparams.h
+++ src/chainparams.h
@@ -24,6 +24,7 @@ struct SeedSpec6 {
 };
 
 typedef std::map<int, uint256> MapCheckpoints;
+typedef std::map<int, int> MapFutureBlockTimeWindows;
 
 struct CCheckpointData {
     MapCheckpoints mapCheckpoints;
@@ -122,7 +123,7 @@ public:
     int GetLastCommunityFeeBlockHeight() const { return vCommunityFeeLastHeight; }
     /** Enforce coinbase consensus rule in regtest mode */
     void SetRegTestCoinbaseMustBeProtected() { consensus.fCoinbaseMustBeProtected = true; }
-    int GetNewTimeRule() const { return newTimeRule; }
+    int GetFutureBlockTimeWindow(int height) const;
 protected:
     CChainParams() {}
 
@@ -154,7 +155,7 @@ protected:
     std::vector<std::string> vCommunityFeeAddress;
     int vCommunityFeeStartHeight;
     int vCommunityFeeLastHeight;
-    int newTimeRule;
+    MapFutureBlockTimeWindows futureBlockTimeWindows;
 };
 
 /**
